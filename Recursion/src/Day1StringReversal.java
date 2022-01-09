bdaf8dafc8b7659479eb66ddbd05f327b854f60d
Deepti Swain <deeptyswain.ece@gmail.com>
2022-01-10 02:11:14 +0530

    Day1: Recursion: Palindrome String

--- Recursion/src/Day1StringReversal.java
+++ Recursion/src/Day1StringReversal.java
@@ -3,34 +3,38 @@ import java.util.Scanner;
 /**
  * Problem 1: Reverse a given String.
  * Approach 1: To solve the string reversal using the Recursion,
- we need to first think, what would be the base condition to stop the recursion. :
- 1. could be empty string.
- 2. string.length() = 1;
- - We need to find out the solution which will smaller the input to solve the problem.
- 1. breaking the String into substring(1)[which means it will return a string  from index 1 and reduces gradually 1  by 1 index value] of the string each time
- and concatenate it to the previous part of the string.
- Eg: Input: Apple
- Our Approach: recursiveFunction(String.subString(1)) + String.charAt(1) --->
- till String length is 0 i.e string is ""
+ * we need to first think, what would be the base condition to stop the recursion. :
+ * 1. could be empty string.
+ * 2. string.length() = 1;
+ * - We need to find out the solution which will smaller the input to solve the problem.
+ * 1. breaking the String into substring(1)[which means it will return a string  from index 1 and reduces gradually 1  by 1 index value] of the string each time
+ * and concatenate it to the previous part of the string.
+ * Eg: Input: Apple
+ * Our Approach: recursiveFunction(String.subString(1)) + String.charAt(1) --->
+ * till String length is 0 i.e string is "
+ *
+ * @author Deepti Swain
  */
 
 public class Day1StringReversal {
     //Reverse Logic using recursion:
-    public String reverseString(String str){
-        if(str.length() == 0){  // we also can have condition as if(str.equals(""))
+    public String reverseString(String str) {
+        // Base case / Stopping Condition:
+        if (str.length() == 0) {  // we also can have condition as if(str.equals(""))
             return "";
         }
+        // recursive case:
         return reverseString(str.substring(1)) + str.charAt(0);
     }
 
     // Driver Code:
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // Taking user input:
         Scanner scan = new Scanner(System.in);
         System.out.println("Provide the String for Reverse: ");
         String inputStr = scan.next();
         // calling reverseString method:
-        Day1StringReversal dsr = new  Day1StringReversal();
+        Day1StringReversal dsr = new Day1StringReversal();
         System.out.println(dsr.reverseString(inputStr));
     }
 }
---
+++ Recursion/src/PalindromeString.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+/**
+ * Problem 2: Check if a given String is a palindrome or not?
+ * eg 1: POP --> if we reverse the string it remains same as original string i.e POP : So return true
+ * eg 2: KayaK --> True (Since, reverse is true)
+ * eg 3: kayaK -> false
+ * eg 3: Good ---> False (Since, reverse is dooG not same as original String.
+ * <p>
+ * Approach: Base condition: String length is 0 or 1  then return true.
+ * <p>
+ * if char at 0 is same as char at length()-1 ---> then true
+ * ---> call the recursive function with smaller input by substring from next char i.e 1 and remaining str.length()-1
+ *
+ * @author Deepti Swain
+ */
+public class PalindromeString {
+    // Function to check the string is Palindrome or not.
+    public boolean isPalindrome(String str) {
+        // Base condition / stopping condition:
+        if (str.length() == 0 || str.length() == 1) {
+            return true;
+        }
+        // recursive case:
+        if (str.charAt(0) == str.charAt(str.length() - 1)) { //k = k, a= a
+            return isPalindrome(str.substring(1, str.length() - 1)); //a=a so on
+        }
+        return false;
+    }
+
+    // Driver Code:
+    public static void main(String[] args) {
+        // Taking user input:
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Provide the String To Check Palindrome Or Not: ");
+        String inputStr = scan.next();
+        // calling PalindromeString method:
+        PalindromeString palDrmStr = new PalindromeString();
+        System.out.println(palDrmStr.isPalindrome(inputStr));
+    }
+}
